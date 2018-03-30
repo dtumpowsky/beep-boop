391f60a0bb4355998aa4133aeb50e48b178ed5ad
David Tumpowsky <tumpowskydavid@gmail.com>
2018-03-30 13:33:19 -0700

    draft potential logic to be used

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,32 @@
-
-
-
+var numberArr = []
+var arrLength;
 
 $(document).ready(function() {
   $("form#hal").submit(function(event) {
   event.preventDefault();
 
   var number = $("input#userInt").val();
-  var numberInt = parseInt(numberInt);
+  var numberInt = parseInt(number);
+  var numberArr = [numberInt];
+  console.log(numberArr)
+  console.log(numberArr.length)
+
+
+
+  for (i=0, i<numberArr.length, i++) {
+    number
+    if (numberInt = numberArr.length) {
+      arrLength = 
+    } else {
+      .pop(numberInt--)
+    }
+  }
+
+
+
 
   $("#result").text(number);
+
 
   });
 });
